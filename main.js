e1bcee842c5576f9e64a93f56127855f81180509
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-08-02 19:35:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var P1 = [];
+var P2 = [];
+function Start(){
+    document.getElementById("P1").innerHTML =document.getElementById("T1").value + document.getElementById("T2").value + document.getElementById("T3").value + document.getElementById("T4").value + document.getElementById("T5").value + document.getElementById("T6").value + document.getElementById("T7").value + document.getElementById("T8").value;
+    document.getElementById("P2").innerHTML =document.getElementById("T9").value + document.getElementById("T10").value + document.getElementById("T11").value + document.getElementById("T12").value + document.getElementById("T13").value + document.getElementById("T14").value + document.getElementById("T15").value + document.getElementById("T16").value;
+}
